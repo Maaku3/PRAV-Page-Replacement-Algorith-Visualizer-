7c9f1ac5a7ade6bbd1aa1d0403c0dc74d1210c98
Maaku3 <markjacinto58@gmail.com>
2025-05-19 12:10:41 +0800

    Add project files.

---
+++ page replacement/PGA.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        txtPages = New TextBox()
+        txtFrames = New TextBox()
+        btnFIFO = New Button()
+        btnLRU = New Button()
+        btnOptimal = New Button()
+        lstFIFO = New ListBox()
+        lblFrames = New Label()
+        lblPages = New Label()
+        btnGenerate = New Button()
+        lstLRU = New ListBox()
+        lstOptimal = New ListBox()
+        lblFIFO = New Label()
+        lblLRU = New Label()
+        lblOptimal = New Label()
+        rtbProcess = New RichTextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' txtPages
+        ' 
+        txtPages.Location = New Point(395, 30)
+        txtPages.Name = "txtPages"
+        txtPages.Size = New Size(375, 23)
+        txtPages.TabIndex = 0
+        ' 
+        ' txtFrames
+        ' 
+        txtFrames.Location = New Point(13, 30)
+        txtFrames.Name = "txtFrames"
+        txtFrames.Size = New Size(376, 23)
+        txtFrames.TabIndex = 1
+        ' 
+        ' btnFIFO
+        ' 
+        btnFIFO.Location = New Point(13, 59)
+        btnFIFO.Name = "btnFIFO"
+        btnFIFO.Size = New Size(185, 40)
+        btnFIFO.TabIndex = 2
+        btnFIFO.Text = "Run FIFO"
+        btnFIFO.UseVisualStyleBackColor = True
+        ' 
+        ' btnLRU
+        ' 
+        btnLRU.Location = New Point(204, 59)
+        btnLRU.Name = "btnLRU"
+        btnLRU.Size = New Size(185, 40)
+        btnLRU.TabIndex = 3
+        btnLRU.Text = "Run LRU"
+        btnLRU.UseVisualStyleBackColor = True
+        ' 
+        ' btnOptimal
+        ' 
+        btnOptimal.Location = New Point(395, 59)
+        btnOptimal.Name = "btnOptimal"
+        btnOptimal.Size = New Size(185, 40)
+        btnOptimal.TabIndex = 4
+        btnOptimal.Text = "Run Optimal"
+        btnOptimal.UseVisualStyleBackColor = True
+        ' 
+        ' lstFIFO
+        ' 
+        lstFIFO.FormattingEnabled = True
+        lstFIFO.ItemHeight = 15
+        lstFIFO.Location = New Point(11, 120)
+        lstFIFO.Name = "lstFIFO"
+        lstFIFO.Size = New Size(250, 199)
+        lstFIFO.TabIndex = 5
+        ' 
+        ' lblFrames
+        ' 
+        lblFrames.AutoSize = True
+        lblFrames.Location = New Point(13, 12)
+        lblFrames.Name = "lblFrames"
+        lblFrames.Size = New Size(109, 15)
+        lblFrames.TabIndex = 6
+        lblFrames.Text = "Number of Frames:"
+        ' 
+        ' lblPages
+        ' 
+        lblPages.AutoSize = True
+        lblPages.Location = New Point(395, 12)
+        lblPages.Name = "lblPages"
+        lblPages.Size = New Size(70, 15)
+        lblPages.TabIndex = 7
+        lblPages.Text = "Page String:"
+        ' 
+        ' btnGenerate
+        ' 
+        btnGenerate.Location = New Point(586, 59)
+        btnGenerate.Name = "btnGenerate"
+        btnGenerate.Size = New Size(185, 40)
+        btnGenerate.TabIndex = 8
+        btnGenerate.Text = "Generate Pages"
+        btnGenerate.UseVisualStyleBackColor = True
+        ' 
+        ' lstLRU
+        ' 
+        lstLRU.FormattingEnabled = True
+        lstLRU.ItemHeight = 15
+        lstLRU.Location = New Point(266, 120)
+        lstLRU.Name = "lstLRU"
+        lstLRU.Size = New Size(250, 199)
+        lstLRU.TabIndex = 9
+        ' 
+        ' lstOptimal
+        ' 
+        lstOptimal.FormattingEnabled = True
+        lstOptimal.ItemHeight = 15
+        lstOptimal.Location = New Point(521, 120)
+        lstOptimal.Name = "lstOptimal"
+        lstOptimal.Size = New Size(250, 199)
+        lstOptimal.TabIndex = 10
+        ' 
+        ' lblFIFO
+        ' 
+        lblFIFO.AutoSize = True
+        lblFIFO.Location = New Point(11, 102)
+        lblFIFO.Name = "lblFIFO"
+        lblFIFO.Size = New Size(95, 15)
+        lblFIFO.TabIndex = 11
+        lblFIFO.Text = "FIFO Page faults:"
+        ' 
+        ' lblLRU
+        ' 
+        lblLRU.AutoSize = True
+        lblLRU.Location = New Point(266, 102)
+        lblLRU.Name = "lblLRU"
+        lblLRU.Size = New Size(92, 15)
+        lblLRU.TabIndex = 12
+        lblLRU.Text = "LRU Page faults:"
+        ' 
+        ' lblOptimal
+        ' 
+        lblOptimal.AutoSize = True
+        lblOptimal.Location = New Point(521, 102)
+        lblOptimal.Name = "lblOptimal"
+        lblOptimal.Size = New Size(114, 15)
+        lblOptimal.TabIndex = 13
+        lblOptimal.Text = "Optimal Page faults:"
+        ' 
+        ' rtbProcess
+        ' 
+        rtbProcess.Location = New Point(10, 344)
+        rtbProcess.Name = "rtbProcess"
+        rtbProcess.ReadOnly = True
+        rtbProcess.ScrollBars = RichTextBoxScrollBars.Vertical
+        rtbProcess.Size = New Size(760, 200)
+        rtbProcess.TabIndex = 14
+        rtbProcess.Text = ""
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(11, 326)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(50, 15)
+        Label1.TabIndex = 15
+        Label1.Text = "Process:"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(782, 556)
+        Controls.Add(Label1)
+        Controls.Add(rtbProcess)
+        Controls.Add(lblOptimal)
+        Controls.Add(lblLRU)
+        Controls.Add(lblFIFO)
+        Controls.Add(lstOptimal)
+        Controls.Add(lstLRU)
+        Controls.Add(btnGenerate)
+        Controls.Add(lblPages)
+        Controls.Add(lblFrames)
+        Controls.Add(lstFIFO)
+        Controls.Add(btnOptimal)
+        Controls.Add(btnLRU)
+        Controls.Add(btnFIFO)
+        Controls.Add(txtFrames)
+        Controls.Add(txtPages)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = """FIFO vs LRU vs Optimal Page Replacement"""
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtPages As TextBox
+    Friend WithEvents txtFrames As TextBox
+    Friend WithEvents btnFIFO As Button
+    Friend WithEvents btnLRU As Button
+    Friend WithEvents btnOptimal As Button
+    Friend WithEvents lstFIFO As ListBox
+    Friend WithEvents lblFrames As Label
+    Friend WithEvents lblPages As Label
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents lstLRU As ListBox
+    Friend WithEvents lstOptimal As ListBox
+    Friend WithEvents lblFIFO As Label
+    Friend WithEvents lblLRU As Label
+    Friend WithEvents lblOptimal As Label
+    Friend WithEvents rtbProcess As RichTextBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ page replacement/PGA.vb
@@ -0,0 +1,200 @@
+﻿' This program shows how three page replacement strategies work: FIFO, LRU, and Optimal.
+' It's designed with a GUI so that users can see the process of each method and how many page faults occur.
+
+Public Class Form1
+    ' Array to store the page reference string
+    Dim pageReference() As Integer
+
+    ' Button to generate a random sequence of page requests
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim rand As New Random()
+        pageReference = New Integer(19) {} ' Make an array with 20 elements
+
+        ' Fill the array with random numbers between 0 and 9
+        For i = 0 To pageReference.Length - 1
+            pageReference(i) = rand.Next(0, 10)
+        Next
+
+        ' Display the sequence in a textbox
+        txtPages.Text = String.Join(" ", pageReference)
+        rtbProcess.Clear()
+        rtbProcess.AppendText("Generated Pages: " & txtPages.Text & vbCrLf)
+    End Sub
+
+    ' FIFO button click - runs FIFO algorithm
+    Private Sub btnFIFO_Click(sender As Object, e As EventArgs) Handles btnFIFO.Click
+        If Not ValidateInputs() Then Exit Sub
+
+        Dim frames As Integer = Integer.Parse(txtFrames.Text)
+        Dim faults As Integer = RunFIFO(pageReference, frames, lstFIFO, rtbProcess)
+        lblFIFO.Text = "FIFO Page Faults: " & faults
+    End Sub
+
+    ' LRU button click - runs LRU algorithm
+    Private Sub btnLRU_Click(sender As Object, e As EventArgs) Handles btnLRU.Click
+        If Not ValidateInputs() Then Exit Sub
+
+        Dim frames As Integer = Integer.Parse(txtFrames.Text)
+        Dim faults As Integer = RunLRU(pageReference, frames, lstLRU, rtbProcess)
+        lblLRU.Text = "LRU Page Faults: " & faults
+    End Sub
+
+    ' Optimal button click - runs Optimal algorithm
+    Private Sub btnOptimal_Click(sender As Object, e As EventArgs) Handles btnOptimal.Click
+        If Not ValidateInputs() Then Exit Sub
+
+        Dim frames As Integer = Integer.Parse(txtFrames.Text)
+        Dim faults As Integer = RunOptimal(pageReference, frames, lstOptimal, rtbProcess)
+        lblOptimal.Text = "Optimal Page Faults: " & faults
+    End Sub
+
+    ' allows manual typing of page reference string
+    Private Function ValidateInputs() As Boolean
+        If txtFrames.Text = "" Then
+            MessageBox.Show("Enter the number of frames.")
+            Return False
+        End If
+
+        If txtPages.Text = "" Then
+            MessageBox.Show("Enter or generate the page sequence.")
+            Return False
+        End If
+
+        Dim temp As Integer
+        If Not Integer.TryParse(txtFrames.Text, temp) Then
+            MessageBox.Show("Number of frames must be a number.")
+            Return False
+        End If
+
+        ' Try parsing the typed/entered page string
+        Try
+            pageReference = txtPages.Text.Split({" ", ",", ";"}, StringSplitOptions.RemoveEmptyEntries).Select(Function(s) Integer.Parse(s.Trim())).ToArray()
+        Catch ex As Exception
+            MessageBox.Show("Invalid page string. Please enter space-separated integers (e.g., 0 1 2 3).")
+            Return False
+        End Try
+
+        Return True
+    End Function
+
+    ' FIFO: Replaces the oldest page in memory when full
+    Private Function RunFIFO(pages As Integer(), frames As Integer, lst As ListBox, rtb As RichTextBox) As Integer
+        lst.Items.Clear()
+        rtb.Clear()
+        rtb.AppendText("FIFO" & vbCrLf)
+
+        Dim queue As New Queue(Of Integer)()
+        Dim pageFaults As Integer = 0
+
+        For Each page In pages
+            rtb.AppendText("Checking page: " & page & vbCrLf)
+
+            If Not queue.Contains(page) Then
+                If queue.Count = frames Then
+                    Dim removed = queue.Dequeue()
+                    rtb.AppendText("Removed oldest page: " & removed & vbCrLf)
+                End If
+                queue.Enqueue(page)
+                pageFaults += 1
+                lst.Items.Add("Page " & page & ": (Miss) -> " & String.Join(" ", queue))
+                rtb.AppendText("(Miss) Added to frame: " & page & vbCrLf)
+            Else
+                lst.Items.Add("Page " & page & ": (Hit) -> " & String.Join(" ", queue))
+                rtb.AppendText("(Hit) Already in memory." & vbCrLf)
+            End If
+
+            rtb.AppendText("Frame now: " & String.Join(" ", queue) & vbCrLf & vbCrLf)
+        Next
+
+        rtb.AppendText("Total FIFO Page Faults: " & pageFaults & vbCrLf)
+        Return pageFaults
+    End Function
+
+    ' LRU: Removes the least recently used page when full
+    Private Function RunLRU(pages As Integer(), frames As Integer, lst As ListBox, rtb As RichTextBox) As Integer
+        lst.Items.Clear()
+        rtb.Clear()
+        rtb.AppendText("LRU" & vbCrLf)
+
+        Dim cache As New List(Of Integer)()
+        Dim pageFaults As Integer = 0
+
+        For Each page In pages
+            rtb.AppendText("Checking page: " & page & vbCrLf)
+
+            If cache.Contains(page) Then
+                cache.Remove(page)
+                cache.Add(page)
+                lst.Items.Add("Page " & page & ": (Hit) -> " & String.Join(" ", cache))
+                rtb.AppendText("(Hit) Moved to most recent" & vbCrLf)
+            Else
+                If cache.Count = frames Then
+                    Dim removed = cache(0)
+                    cache.RemoveAt(0)
+                    rtb.AppendText("Removed least recent: " & removed & vbCrLf)
+                End If
+                cache.Add(page)
+                pageFaults += 1
+                lst.Items.Add("Page " & page & ": (Miss) -> " & String.Join(" ", cache))
+                rtb.AppendText("(Miss) Added to memory" & vbCrLf)
+            End If
+
+            rtb.AppendText("Frame now: " & String.Join(" ", cache) & vbCrLf & vbCrLf)
+        Next
+
+        rtb.AppendText("Total LRU Page Faults: " & pageFaults & vbCrLf)
+        Return pageFaults
+    End Function
+
+    ' Optimal: Removes the page that won't be used for the longest time
+    Private Function RunOptimal(pages As Integer(), frames As Integer, lst As ListBox, rtb As RichTextBox) As Integer
+        lst.Items.Clear()
+        rtb.Clear()
+        rtb.AppendText("Optimal" & vbCrLf)
+
+        Dim memory As New List(Of Integer)()
+        Dim pageFaults As Integer = 0
+
+        For i As Integer = 0 To pages.Length - 1
+            Dim page = pages(i)
+            rtb.AppendText("Checking page: " & page & vbCrLf)
+
+            If Not memory.Contains(page) Then
+                If memory.Count < frames Then
+                    memory.Add(page)
+                    rtb.AppendText("Added to free space: " & page & vbCrLf)
+                Else
+                    Dim farthestIndex As Integer = -1
+                    Dim toRemove As Integer = -1
+
+                    ' Look into future use of each page to decide which to replace
+                    For Each mPage In memory
+                        Dim index = Array.IndexOf(pages, mPage, i + 1)
+                        If index = -1 Then
+                            toRemove = mPage
+                            Exit For ' Best option: not used again
+                        ElseIf index > farthestIndex Then
+                            farthestIndex = index
+                            toRemove = mPage
+                        End If
+                    Next
+
+                    memory.Remove(toRemove)
+                    memory.Add(page)
+                    rtb.AppendText("Removed: " & toRemove & " Added: " & page & vbCrLf)
+                End If
+                pageFaults += 1
+                lst.Items.Add("Page " & page & ": (Miss) -> " & String.Join(" ", memory))
+                rtb.AppendText("(Miss) Added to memory" & vbCrLf)
+            Else
+                lst.Items.Add("Page " & page & ": (Hit) -> " & String.Join(" ", memory))
+                rtb.AppendText("(Hit) Page is already present" & vbCrLf)
+            End If
+
+            rtb.AppendText("Frame now: " & String.Join(" ", memory) & vbCrLf & vbCrLf)
+        Next
+
+        rtb.AppendText("Total Optimal Page Faults: " & pageFaults & vbCrLf)
+        Return pageFaults
+    End Function
+End Class
